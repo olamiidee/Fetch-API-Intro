55b5ce0eba158ebdaca93ad0327fa6069870168f
olamiidee <osibeluwoolamide@gmail.com>
2022-10-13 03:49:56 +0100

    post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Grabbing text files
+
 document.getElementById("get-text").addEventListener("click", getText);
 function getText(){
     fetch("sample.txt")
@@ -28,4 +29,44 @@ function getUsers(){
         })
         document.getElementById("output").innerHTML = HTML
     })
+}
+
+// Grabbing API DATA
+
+document.getElementById("get-posts").addEventListener("click", getPosts)
+function getPosts(){
+    fetch ("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=> res.json())
+    .then((data)=> {
+        let HTML = "<h2> Posts </h2>"
+        data.forEach((post)=>{
+            HTML += `
+            <div>
+                <h3> ${post.title} </h3>
+                <p> ${post.body} </p>
+            </div>
+            `
+        })
+        document.getElementById("output").innerHTML = HTML
+    })
+}
+
+// Posting
+
+document.getElementById("add-post").addEventListener("submit", addPost)
+function addPost(e){
+    e.preventDefault
+    let title = document.getElementById("title").value;
+    let body = document.getElementById("body").value;
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json, text/plain, */*",
+            "Content-type": "application/json"
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then((res)=> res.json())
+    .then((data)=> console.log(data))
 }
